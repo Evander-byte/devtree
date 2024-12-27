26d446ac8fce10505ff2683e15eadb33616da07c
Evander-byte <emondragon571@gmail.com>
2024-12-27 12:12:37 -0600

    ConectionDB and model generated

---
+++ src/router.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import { createAccount } from './handlers'
+
+const router = Router()
+
+/* Auth and register */
+
+router.post('/auth/register', createAccount)
+
+export default router
