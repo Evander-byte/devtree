8b3874ef3a252b52dc0cf8b8307f41198dd54ff3
Evander-byte <emondragon571@gmail.com>
2025-01-01 16:59:20 -0600

    Handle register and hash passwords

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -1,10 +1,23 @@
 import type { Request, Response } from 'express'
 import User from "../models/User"
+import { ashPwd } from '../utils/auth'
 
 export const createAccount = async (req: Request, res: Response) => {
-    const user = new User(req.body)
 
+    const {email, password} = req.body
+    
+    const userExist = await User.findOne({email})
+    if(userExist){
+        const error = new Error('El usuario ya esta registrado')
+        res.status(409).json({error: error.message})
+        return
+    }
+    
+    const user = new User(req.body)
+    user.password = await ashPwd(password)
+    
     await user.save()
 
-    res.send('Registro creado exitosamente')
+    res.status(200).send('Registro creado correctamente')
+
 }
